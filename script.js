bb34c1d72e5e245b8015b2e8505cd4ae69bac8f9
cts93 <san.thi@live.com>
2022-10-28 17:05:47 -0300

    Creating script functions, var and tag ref onclick

---
+++ script.js
@@ -0,0 +1,14 @@
+var hour = 0;
+var minute = 0;
+var second = 0;
+
+var time = 1000; //Quantos milésimos valem 1 segundo.
+var cron;
+
+function start() {}
+
+function pause() {}
+
+function stop() {}
+
+function timer() {}
